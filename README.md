f44a552d5afd5fb4cb96b834c9c39e56ade58318
Markus Pfeifer <mkspfeifer@gmail.com>
2022-12-27 00:55:28 +0100

    implemented write to csv, updated readme with csv instructions

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Usage:
 1. Start the application. 
 2. Select a mp4 file.
 3. Select an object you want to track by holding the left mouse button pressed and create a bounding box.
+
+The application then creates a csv file in the directory of the video.
+The csv file has the following structure:
+
+* Column 1: Index of the frame beginning at 0.
+* Column 2: X-coordinate of the center pixel of the tracked object.
+* Column 3: Y-coordinate of the center pixel of the tracked object.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.9
 
 import cv2
+import os
+import csv
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
@@ -18,23 +20,29 @@ def main():
     bbox = cv2.selectROI("img", img, showCrosshair=True)
     tracker.init(img, bbox)
 
-    try:
-        while cv2.getWindowProperty("img", 1) != -1:
-            success, img = vid.read()
-            if success:
-                success, bbox = tracker.update(img)
+    with open(filename.replace(".mp4", ".csv"), 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        csv_writer.writerow(["index of frame", "x pixel coordinate", "y pixel coordinate"])
+        try:
+            frame_idx = 0
+            while cv2.getWindowProperty("img", 1) != -1:
+                success, img = vid.read()
                 if success:
-                    img = cv2.rectangle(img, rec=bbox, color=(255, 0, 0))
+                    success, bbox = tracker.update(img)
+                    if success:
+                        img = cv2.rectangle(img, rec=bbox, color=(255, 0, 0))
+                        csv_writer.writerow([frame_idx, bbox[0] + bbox[1]/2, bbox[2] + bbox[3]/2])
+                    else:
+                        break
+                    cv2.imshow("img", img)
+                    cv2.waitKey(1)
                 else:
                     break
-                cv2.imshow("img", img)
-                cv2.waitKey(1)
-            else:
-                break
-    except cv2.error:
-        print("Application was closed by the user.")
-    finally:
-        cv2.destroyAllWindows()
+                frame_idx += 1
+        except cv2.error:
+            print("Application was closed by the user.")
+        finally:
+            cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
