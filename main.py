9e17e7abbe037dcb5895a61dad92bcca04074d56
Markus Pfeifer <mkspfeifer@gmail.com>
2022-12-29 04:27:39 +0100

    implemented scale

--- main.py
+++ main.py
@@ -6,14 +6,62 @@ import numpy as np
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from collections.abc import Generator
+import math
 
 
 class Image:
-    def __init__(self, pixels: np.ndarray, time: float, center: tuple[int, int] = None, bbox: tuple[int] = None):
+    def __init__(self, pixels: np.ndarray,
+                 time: float,
+                 center: tuple[int, int] = None,
+                 bbox: tuple[int] = None,
+                 pixel_length: float = None):
         self.pixels = pixels
         self.center = center
         self.bbox = bbox
         self.time = time
+        self.pixel_length = pixel_length
+
+        self._line = []
+        self._backup = None
+
+    def _select_pixel(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            if len(self._line) < 2:
+                self._line.append((x, y))
+            else:
+                self.pixels = self._backup.copy()
+                self._line = [(x, y)]
+            if len(self._line) == 2:
+                self._backup = self.pixels.copy()
+                self.pixels = cv2.line(self.pixels, self._line[0], self._line[1], (255, 0, 0), 3)
+            return x, y
+
+    def select_pixels(self):
+        print("Select two points by double clicking, and close this window to continue.")
+        cv2.namedWindow("img")
+        cv2.setMouseCallback("img", self._select_pixel)
+        try:
+            while cv2.getWindowProperty("img", 1) != -1:
+                cv2.imshow("img", self.pixels)
+                cv2.waitKey(1)
+        except cv2.error:
+            pass
+        finally:
+            if len(self._line) != 2:
+                print(f"Not exactly two points were selected! Selected: {self._line}.")
+                cv2.destroyAllWindows()
+                exit(0)
+
+            cv2.imshow("img", self.pixels)
+            cv2.waitKey(1)
+            distance = round(
+                math.sqrt((self._line[0][0] - self._line[1][0]) ** 2 + (self._line[0][1] - self._line[1][1]) ** 2))
+            cm = float(input("Enter length of the marked line in cm: "))
+            self.pixel_length = cm / distance
+
+            self.pixels = self._backup.copy()
+            self._backup = None
+            cv2.destroyAllWindows()
 
 
 class Video:
@@ -40,6 +88,9 @@ class Video:
         return Image(img, time=self._time - self.time_per_image)
 
 
+
+
+
 def main():
     # Define fps.
     fps: float = 100.
@@ -55,9 +106,18 @@ def main():
     video = Video(filename, fps=fps)
     tracker = cv2.TrackerCSRT_create()
 
-    image = video.get_frame().pixels
-    bbox = cv2.selectROI("img", image, showCrosshair=True)
-    tracker.init(image, bbox)
+    # Extract first frame.
+    image = video.get_frame()
+
+    # Get scale.
+    image.select_pixels()
+
+
+
+
+    # Initialize tracker.
+    bbox = cv2.selectROI("img", image.pixels, showCrosshair=True)
+    tracker.init(image.pixels, bbox)
 
     with open(filename.replace(".mp4", ".csv"), 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',')
